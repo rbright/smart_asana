a88a293a6503def00752de79c6b9f627acdcbeed
Ryan Bright <ryan@rbright.net>
2012-05-11 20:59:48 -0400

    Miscellaneous clean-up

--- lib/smart_asana.rb
+++ lib/smart_asana.rb
@@ -1,5 +1,4 @@
 require 'active_support/core_ext/string/inflections'
-
 require "smart_asana/version"
 
 module SmartAsana
@@ -9,60 +8,34 @@ module SmartAsana
 
   class << self
 
-    def parse(args)
-      words = args.split(' ')
-      indicators = indicators(words)
-      name = name(words, indicators)
-      attributes(indicators).tap do |hash|
-        hash[:name] = name
-      end
+    def create_task(attrs)
+      attrs = parse(attrs)
+      workspace = workspace(attrs)
+      attrs.delete(:workspace)
+      workspace.create_task(attrs)
     end
 
     private
 
-      def assignee_status(attr)
-        case attr
-        when '1'
-          'today'
-        when '2'
-          'upcoming'
-        when '3'
-          'later'
-        else
-          'inbox'
+      def workspace(attrs)
+        workspaces = Asana::Workspace.all
+
+        if attrs[:workspace]
+          workspace = workspaces.select { |w|
+            w.name.downcase == attrs[:workspace].downcase
+          }.first
+          return workspace unless !workspace
         end
+
+        workspaces.first
       end
 
-      def attributes(indicators)
-        attrs = {}.tap do |array|
-          indicators.each do |ind|
-            attr = ind[1..ind.length]
-            case ind[0]
-            when '+'
-              array[:assignee_status] = assignee_status(attr)
-            when '#'
-              array[:workspace] = attr.titleize
-            when '^'
-              array[:due_on] = due_on(attr)
-            end
-          end
-        end
-      end
-
-      def due_on(attr)
-        case attr
-        when 'today'
-          Date.today
-        when 'tomorrow'
-          Date.today.next_day
-        else
-          if DAYS_OF_WEEK.include?(attr)
-            1.upto(7).each do |i|
-              date = Date.today.next_day(i)
-              return date if date.send("#{attr}?".to_sym)
-            end
-          end
-        end
+      def parse(words)
+        indicators = indicators(words)
+        name = name(words, indicators)
+        attributes(indicators).tap { |hash|
+          hash[:name] = name
+        }
       end
 
       def indicators(words)
@@ -83,6 +56,50 @@ module SmartAsana
         }.join(' ')
       end
 
-  end
+      def attributes(indicators)
+        attrs = {}.tap { |array|
+          indicators.each do |ind|
+            attr = ind[1..ind.length]
+            case ind[0]
+            when '+'
+              array[:assignee_status] = assignee_status(attr)
+            when '#'
+              array[:workspace] = attr.titleize
+            when '^'
+              array[:due_on] = due_on(attr)
+            end
+          end
+        }
+      end
 
+      def assignee_status(attr)
+        case attr
+        when '1'
+          'today'
+        when '2'
+          'upcoming'
+        when '3'
+          'later'
+        else
+          'inbox'
+        end
+      end
+
+      def due_on(attr)
+        case attr
+        when 'today'
+          Date.today
+        when 'tomorrow'
+          Date.today.next_day
+        else
+          if DAYS_OF_WEEK.include?(attr)
+            1.upto(7).each do |i|
+              date = Date.today.next_day(i)
+              return date if date.send("#{attr}?".to_sym)
+            end
+          end
+        end
+      end
+
+  end
 end
